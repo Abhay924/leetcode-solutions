3f9bc99caf7f2ff8c880f5f76b59bd91ded8fb6b
Abhay Kushwaha <abhaykushawaha06@gmail.com>
2026-07-12 23:29:47 +0530

    Time: 340 ms (99.76%), Space: 0B (100%) - LeetHub

---
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
