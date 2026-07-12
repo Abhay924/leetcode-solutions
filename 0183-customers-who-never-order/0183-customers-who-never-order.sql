caf33cd42141eea2b23c752b3a033b8eba74906d
Abhay Kushwaha <abhaykushawaha06@gmail.com>
2026-07-12 23:41:55 +0530

    Time: 547 ms (85.05%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.id IS NULL;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
