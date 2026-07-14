12b6d70eeae43f169552a4b4d34489c5e1f2430b
Abhay Kushwaha <abhaykushawaha06@gmail.com>
2026-07-14 22:34:39 +0530

    Time: 382 ms (41.67%), Space: 276.4 MB (25%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.java
@@ -3,20 +3,15 @@ class Solution {
     private int[][][] dp;
 
     public int subsequencePairCount(int[] nums) {
-
         dp = new int[nums.length][201][201];
-
         for (int[][] x : dp) {
             for (int[] y : x) {
                 Arrays.fill(y, -1);
             }
         }
-
         return dfs(nums, 0, 0, 0);
     }
-
     private int dfs(int[] nums, int idx, int g1, int g2) {
-
         if (idx == nums.length) {
             return (g1 != 0 && g2 != 0 && g1 == g2) ? 1 : 0;
         }
@@ -25,11 +20,9 @@ class Solution {
             return dp[idx][g1][g2];
 
         long ans = 0;
-
         ans += dfs(nums, idx + 1, gcd(g1, nums[idx]), g2);
         ans += dfs(nums, idx + 1, g1, gcd(g2, nums[idx]));
         ans += dfs(nums, idx + 1, g1, g2);
-
         return dp[idx][g1][g2] = (int)(ans % MOD);
     }
 
@@ -43,3 +36,7 @@ class Solution {
         return a;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
