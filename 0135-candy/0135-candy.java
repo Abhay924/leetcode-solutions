39108af16703ba8ada879a36ea9a1c9b9e1a5d23
Abhay Kushwaha <abhaykushawaha06@gmail.com>
2026-08-23 22:57:20 +0530

    Time: 3 ms (86.39%), Space: 52.5 MB (6.9%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int result = 0;
+        for (int candy : candies) {
+            result += candy;
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
