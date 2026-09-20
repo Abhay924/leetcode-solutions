63d387d32cd612ad28fb15e6ae9b31db9ed825c5
Abhay Kushwaha <abhaykushawaha06@gmail.com>
2026-09-20 10:27:05 +0530

    Time: 1 ms (100%), Space: 43.7 MB (91.21%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int reverseValue = 26 - (s.charAt(i) - 'a');
+            sum += reverseValue * (i + 1);
+        }
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
